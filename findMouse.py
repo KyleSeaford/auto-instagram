c97a311717e8bb5fe26d1ddbda76cd46f5719730
Kyle seaford <147045805+KyleSeaford@users.noreply.github.com>
2023-12-22 15:09:21 -0800

    Add files via upload

---
+++ findMouse.py
@@ -0,0 +1,13 @@
+import time
+import pyautogui
+
+def findMouse():
+    # find screen size
+    print(pyautogui.size())
+    time.sleep(4)
+
+    # find mouse position
+    print(pyautogui.position())
+
+findMouse()
+
